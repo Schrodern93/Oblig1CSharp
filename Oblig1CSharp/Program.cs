c445ec9268c1e937403da61152b11210a30cdb67
Joakim Nikolai Schrøder <55440505+Schrodern93@users.noreply.github.com>
2020-01-29 08:46:02 +0100

    Initial commit

---
+++ Oblig1CSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Oblig1CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
