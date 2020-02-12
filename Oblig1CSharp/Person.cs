89aa433a6630ff7b78062f19d5f568cd482d202b
Joakim Nikolai Schrøder <55440505+Schrodern93@users.noreply.github.com>
2020-02-12 09:55:00 +0100

    x

--- Oblig1CSharp/Person.cs
+++ Oblig1CSharp/Person.cs
@@ -2,60 +2,56 @@
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace Oblig1CSharp
 {
     class Person
     {
         public int Id { get; set; }
-        public string FirstName { get; private set; }
-        public int BirthYear { get; private set; }
-        public string LastName { get; private set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Person Mother { get; set; }
+        public Person Father { get; set; }
+        public int BirthYear { get; set; }
+        public int DeathYear { get; set; }
 
-        public static object Informasjon()
+        public void Show()
         {
+            var death = "";
+            var mother = "";
+            var father = "";
 
-            var sverreMagnus = new Person { Id = 1, FirstName = "Sverre Magnus", BirthYear = 2005 };
-            var ingridAlexandra = new Person { Id = 2, FirstName = "Ingrid Alexandra", BirthYear = 2004 };
-            var haakon = new Person { Id = 3, FirstName = "Haakon Magnus", BirthYear = 1973 };
-            var metteMarit = new Person { Id = 4, FirstName = "Mette-Marit", BirthYear = 1973 };
-            var marius = new Person { Id = 5, FirstName = "Marius", LastName = "Borg Høiby", BirthYear = 1997 };
-            var harald = new Person { Id = 6, FirstName = "Harald", BirthYear = 1937 };
-            var sonja = new Person { Id = 7, FirstName = "Sonja", BirthYear = 1937 };
-            var olav = new Person { Id = 8, FirstName = "Olav", BirthYear = 1903 };
+            if (DeathYear != 0)
+            {
 
+                death = Convert.ToString("DeathYear: " + DeathYear);
+            }
+            if (Mother != null)
+            {
+                mother = "Mor: Id: " + Convert.ToString(Mother.Id) + " Name: " + Mother.FirstName + " " + Mother.LastName;
+            }
+            if (Father != null)
+            {
+                father = "Far: Id: " + Convert.ToString(Father.Id) + " Name: " + Father.FirstName + " " + Father.LastName;
+            }
+            Console.WriteLine($"Id: {Id} Name: {FirstName} {LastName} BirthYear: {BirthYear} {death}  {mother} {father} ");
 
-
-            //sverreMagnus.Father = haakon;
-            //sverreMagnus.Mother = metteMarit;
-            //ingridAlexandra.Father = haakon;
-            //ingridAlexandra.Mother = metteMarit;
-            //marius.Mother = metteMarit;
-            //haakon.Father = harald;
-            //haakon.Mother = sonja;
-            //harald.Father = olav;
-
-            string liste = Convert.ToString(sverreMagnus);
-            Console.WriteLine(liste);
-            return liste;
         }
-        public static int FinnId(int id)
+        public void Barn(int id, List<Person> liste)
         {
+            var count = 0;
+            //List<Person> = new List<>
+            foreach (var person in liste)
+            {
 
-            return id;
-        }
-        public static string NyPerson(int Id1, string FirstName1, string LastName1, int BirthYear1)
-        {
-            Console.WriteLine("Skriv inn id: ");
-            Id1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Skriv inn fornavn: ");
-            FirstName1 = Console.ReadLine();
-            Console.WriteLine("Skriv inn etternavn: ");
-            LastName1 = Console.ReadLine();
-            Console.WriteLine("Skriv inn fødselsdag: ");
-            BirthYear1 = Convert.ToInt32(Console.ReadLine());
-            var NewPerson = new Person { Id = Id1, FirstName = FirstName1, LastName = LastName1, BirthYear = BirthYear1 };
+                if (person.Father == liste[id - 1] || person.Mother == liste[id - 1])
+                {
 
-            return Convert.ToString(NewPerson);
+                    if (count == 0) Console.WriteLine("Barn:");
+                    Console.WriteLine($"Id: {Convert.ToString(person.Id)} Name: {person.FirstName} {person.LastName} BirthYear: {person.BirthYear} ");
+                    count++;
+                }
+            }
         }
     }
 }
--- Oblig1CSharp/Program.cs
+++ Oblig1CSharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Oblig1CSharp
 {
@@ -6,12 +7,80 @@ namespace Oblig1CSharp
     {
         static void Main(string[] args)
         {
+        
+            // lager en liste av type Person
+            List<Person> personListe = new List<Person>();
 
-            Console.WriteLine("Hello World!");
-            Console.WriteLine(Person.Informasjon());
+            var sverreMagnus = new Person { Id = 1, FirstName = "Sverre Magnus", BirthYear = 2005 };
+            var ingridAlexandra = new Person { Id = 2, FirstName = "Ingrid Alexandra", BirthYear = 2004 };
+            var haakon = new Person { Id = 3, FirstName = "Haakon Magnus", BirthYear = 1973 };
+            var metteMarit = new Person { Id = 4, FirstName = "Mette-Marit", BirthYear = 1973 };
+            var marius = new Person { Id = 5, FirstName = "Marius", LastName = "Borg Høiby", BirthYear = 1997 };
+            var harald = new Person { Id = 6, FirstName = "Harald", BirthYear = 1937 };
+            var sonja = new Person { Id = 7, FirstName = "Sonja", BirthYear = 1937 };
+            var olav = new Person { Id = 8, FirstName = "Olav", BirthYear = 1903 };
+            var gunnar = new Person { Id = 9, FirstName = "Gunnar", LastName = "Nordmann"};
 
-            Console.ReadLine();
-            //Person.FinnId(Convert.ToInt32(Console.ReadLine()));  // denne metoden skal ta in bruker input og gi personen via id (eks "2" er trykket) 
+            sverreMagnus.Father = haakon;
+            sverreMagnus.Mother = metteMarit;
+            ingridAlexandra.Father = haakon;
+            ingridAlexandra.Mother = metteMarit;
+            marius.Mother = metteMarit;
+            haakon.Father = harald;
+            haakon.Mother = sonja;
+            harald.Father = olav;
+
+            // legger til personer i lista (personListe)
+            personListe.Add(sverreMagnus);
+            personListe.Add(ingridAlexandra);
+            personListe.Add(haakon);
+            personListe.Add(metteMarit);
+            personListe.Add(marius);
+            personListe.Add(harald);
+            personListe.Add(sonja);
+            personListe.Add(olav);
+            personListe.Add(gunnar);
+
+            Console.WriteLine("Skriv in (hjelp) for hjelpetekst");
+            Console.WriteLine("Skriv in (stop) for å stoppe programmet");
+            var input = "";
+            while (input != "stop")
+            {
+                input = Console.ReadLine();
+                if (input == "hjelp") Program.Hjelp();
+                else if (input == "liste") Program.Liste(personListe);
+                else if (input == "vis")
+                {
+                    Console.WriteLine("Hvilken Id vil du vise");
+                    var inputId = Convert.ToInt32(Console.ReadLine());
+                    Program.Vis(inputId, personListe);
+                }
+                else Console.WriteLine("Feil kommando");
+            }
         }
+
+        private static void Vis(int inputId, List<Person> person)
+        {
+
+            person[inputId - 1].Show();
+            person[inputId - 1].Barn(inputId, person);
+        }
+
+        private static void Liste(List<Person> liste)
+        {
+            foreach (var personer in liste)
+            {
+                personer.Show();
+            }
+        }
+
+        public static void Hjelp()
+        {
+            Console.WriteLine("Skriv inn (vis) for å vise en bestemt person");
+            Console.WriteLine("Skriv inn (liste) for å vise alle personer");
+        }
+
+
+
     }
 }
